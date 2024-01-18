d1d23ef50b37bc92c0fab3f7db23a5b848dca4c8
Jarlath O'Neill <30296432+JarlathONeill@users.noreply.github.com>
2024-01-18 13:02:56 +0000

    Alternative take on web dev assignment using expressrunclub as the template

---
+++ controllers/schedulecontrollers.js
@@ -0,0 +1,102 @@
+const conn = require('./../utils/dbconn');
+
+exports.getSchedule = (req, res) => {
+    const selectSQL = 'SELECT * FROM emotiondata';
+    conn.query(selectSQL, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            res.render('index', { schedule: rows});
+        }
+    });
+};
+
+exports.getAddNewRun =  (req, res) => {
+    res.render('addsnapshot');
+    console.log(req.body);
+};
+
+exports.getRegister = (req, res) => {
+    res.render('register');
+}
+
+
+exports.selectRun =  (req, res) => {
+    const { id } = req.params;
+
+    const selectSQL = `SELECT * FROM runschedule WHERE id = ${id}`;
+    conn.query(selectSQL, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(rows);
+            res.render('editschedule', { details: rows});
+        }
+    });
+};
+
+exports.postNewUser = (req, res) => {
+    const { email, password} = req.body;
+    const vals = [email, password];
+
+    const insertSQL = 'INSERT into user (email, password) VALUES (?, ?)';
+
+    conn.query(insertSQL, vals, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            res.redirect('/');
+        }
+    });
+}
+
+
+exports.postNewRun = (req, res) => {
+    const { new_context, new_date } = req.body;
+    const vals = [new_context, new_date];
+
+    const insertSQL = 'INSERT INTO emotiondata (context_trigger, date_time_record) VALUES (?, ?)';
+
+    conn.query(insertSQL, vals, (err, rows) => {
+        if(err) {
+            throw err;
+        } else {
+            res.redirect('/');
+        }
+    });
+    console.log(req.body);
+};
+
+
+exports.updateRun = (req, res) => {
+    //console.log(req.params);
+    //console.log(req.body);
+
+    const run_id = req.params.id;
+    const { run_details, run_date } = req.body;
+    const vals = [ run_details, run_date, run_id ];
+    console.log(vals);
+
+    const updateSQL = 'UPDATE runschedule SET items = ?, mydate = ? WHERE id = ?';
+    conn.query(updateSQL, vals, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            res.redirect('/');
+        }
+    });
+};
+
+
+exports.deleteRun = (req, res) => {
+    const run_id = req.params.id;
+
+    const deleteSQL = `DELETE FROM runschedule WHERE id = ${run_id}`;
+    conn.query(deleteSQL, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            res.redirect('/');
+        }
+    });
+};
---
+++ routes/scheduleroutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const controller = require('./../controllers/schedulecontrollers');
+const conn = require('../utils/dbconn');
+const router = express.Router();
+
+router.get('/', controller.getSchedule);
+router.get('/new', controller.getAddNewRun);
+router.get('/register', controller.getRegister);
+router.get('/edit/:id', controller.selectRun);
+router.post('/new', controller.postNewRun);
+router.post('/edit/:id', controller.updateRun);
+router.post('/del/:id', controller.deleteRun);
+
+module.exports = router;
