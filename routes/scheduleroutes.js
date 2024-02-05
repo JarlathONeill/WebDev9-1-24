92dfe315849455cd18107245a3b74c541627d94b
Jarlath O'Neill <30296432+JarlathONeill@users.noreply.github.com>
2024-02-05 19:59:19 +0000

    added routes to addsnapshot.ejs

--- routes/scheduleroutes.js
+++ routes/scheduleroutes.js
@@ -4,19 +4,21 @@ const conn = require('../utils/dbconn');
 const router = express.Router();
 
 router.get('/', controller.getHome);
-router.get('/new', controller.getAddNewRun);
+//router.get('/new', controller.getAddNewRun);
 router.get('/register', controller.getRegister);
-router.get('/edit/:id', controller.selectRun);
+//router.get('/edit/:id', controller.selectRun);
 router.get('/login', controller.getLogin);
 router.get('/logout', controller.getLogout);
 router.get('/dashboard', controller.getDashboard);
+router.get('/addsnapshot', controller.getNewSnap);
 
 
 
-router.post('/new', controller.postNewRun);
-router.post('/edit/:id', controller.updateRun);
-router.post('/del/:id', controller.deleteRun);
+//router.post('/new', controller.postNewRun);
+//router.post('/edit/:id', controller.updateRun);
+//router.post('/del/:id', controller.deleteRun);
 router.post('/register', controller.postRegister);
 router.post('/login', controller.postLogin);
+router.post('/addsnapshot', controller.postNewSnap);
 
 module.exports = router;
